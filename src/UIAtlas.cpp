f1e5402e52235fd3356bebe54bdcd2f4a36f1a8f
Jonathan Haslow-Hall <Jhaslowh@gmail.com>
2014-05-22 20:18:13 -0400

    Removed unused objects from UIAtlas

--- src/UIAtlas.cpp
+++ src/UIAtlas.cpp
@@ -34,67 +34,6 @@ void UIAtlas::load(){
 	vcg->addFrame(655,105,24,24);
 	// Rec
 	vcg->addFrame(14.0f,33.0f,1.0f,1.0f);
-	// Level Arrow
-	vcg->addFrame(732.0f, 1.0f,64.0f,64.0f);
-	// Point
-	vcg->addFrame(680.0f,106.0f,8.0f,8.0f);
-	// Bubble center
-	vcg->addFrame(732.0f,67.0f,1.0f,20.0f);
-	// Bubble edge
-	vcg->addFrame(735.0f,67.0f,10.0f,20.0f);
-	// Bubble arrow
-	vcg->addFrame(747.0f,67.0f,10.0f,10.0f);
-
-	// UI Outline boxes
-	// Top left corner
-	vcg->addFrame(690.0f, 106.0f, 6.0f, 6.0f);
-	// Top right corner 
-	vcg->addFrame(697.0f, 106.0f, 6.0f, 6.0f);
-	// Bottom left corner 
-	vcg->addFrame(690.0f, 113.0f, 6.0f, 6.0f);
-	// Bottom right corner 
-	vcg->addFrame(697.0f, 113.0f, 6.0f, 6.0f);
-	// Top border
-	vcg->addFrame(696.0f, 106.0f, 1.0f, 6.0f);
-	// Bottom border
-	vcg->addFrame(696.0f, 113.0f, 1.0f, 6.0f);
-	// Left border
-	vcg->addFrame(690.0f, 112.0f, 6.0f, 1.0f);
-	// Right border
-	vcg->addFrame(697.0f, 112.0f, 6.0f, 1.0f);
-	// Middle
-	vcg->addFrame(696.0f, 112.0f, 1.0f, 1.0f);
-	// Currency Animal
-	vcg->addFrame(632.0f, 130.0f, 20.0f, 20.0f);
-	// Currency Human
-	vcg->addFrame(654.0f, 130.0f, 20.0f, 20.0f);
-	// Combo box arrow
-	vcg->addFrame(632.0f, 152.0f, 20.0f, 20.0f);
-
-	// LS Buttons
-	// LS Target Light
-	vcg->addFrame(798.0f, 1.0f, 50.0f, 50.0f);
-	// LS Target Dark
-	vcg->addFrame(850.0f, 1.0f, 50.0f, 50.0f);
-	// LS Home
-	vcg->addFrame(902.0f, 1.0f, 50.0f, 50.0f);
-	// LS Target Light Check 
-	vcg->addFrame(798.0f, 53.0f, 50.0f, 50.0f);
-	// LS Target Dark Check 
-	vcg->addFrame(850.0f, 53.0f, 50.0f, 50.0f);
-
-	// Difficulty 1
-	vcg->addFrame(705.0f, 106.0f, 30.0f, 30.0f);
-	// Difficulty 2
-	vcg->addFrame(737.0f, 106.0f, 30.0f, 30.0f);
-	// Difficulty 3
-	vcg->addFrame(769.0f, 106.0f, 30.0f, 30.0f);
-
-	// Tut arrow
-	vcg->addFrame(631.0f, 175.0f, 20.0f, 20.0f);
-
-	// Tut ammo
-	vcg->addFrame(801.0f, 106.0f, 20.0f, 20.0f);
 
 	// Grab the arrays 
 	verts = vcg->getVertices();
